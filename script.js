f90f0f5ce166bf98d7726d286260a2ce8f663d41
andrewchu1357 <116685338+andrewchu1357@users.noreply.github.com>
2026-06-14 22:53:05 -0400

    e

--- script.js
+++ script.js
@@ -98,4 +98,27 @@ document.addEventListener("DOMContentLoaded", function() {
       cookieWindow.style.display = "none";
     }
   };
+
+    // about
+  const about = document.getElementById("about");
+  const aboutHeader = document.getElementById("aboutHeader");
+  if (about && aboutHeader) {
+    dragElement(about, aboutHeader);
+  }
+
+  window.openCookieWindow = function() {
+    if (about) {
+      about.style.display = "flex";
+      about.style.zIndex = 100;
+    }
+  };
+
+  window.closeCookieWindow = function() {
+    if (about) {
+      about.style.display = "none";
+    }
+  };
+
 });
+
+
